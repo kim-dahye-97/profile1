4b8bde51798a1cb977c21f5dc4ef02eca516ab6c
dahyekim_97 <rlaekgp3305@naver.com>
2020-07-09 23:15:40 +0900

    프로젝트 추가 및 순서 변경

--- js/half_scroll.js
+++ js/half_scroll.js
@@ -7,10 +7,16 @@ $(window).load(function () {
     var titLiNum = titLi.size(); //제목 리스트 Li 개수
     var txtLi = $(txt_list).children(); // 텍스트 리스트 Li
     var idx = 0;
-    var windowWidth = $(window).width();
+    var windowWidth
 
 
+    Reset();
     inEvent();
+
+    function Reset() { //window사이즈를 측정해주는 함수
+        windowWidth = $(window).width();
+    }
+
 
     function inEvent() { //이벤트 적용 함수
         $(window).on('resize', Reset);
